2564eb1e9006068ea9ad2584c40c0bedf39f2c2d
cessangeles <130669646+cessangeles@users.noreply.github.com>
2023-09-18 22:07:35 +0800

    database

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,16 +3,40 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-
+// use App\Models\ProductModel;
 class ProductController extends BaseController
 {
+    private $product;
+    public function __construct()
+    {
+        $this->product = new \App\Models\ProductModel();
+    }
+    public function delete($id)
+    {
+        $this->product->delete($id);
+        return redirect()->to('product');
+    }
+    public function save()
+    {
+        $data =[
+        'UPC' => $this->request->getVar('UPC'),
+        'name' => $this->request->getVar('name'),
+        'quantity' => $this->request->getVar('quantity'),
+        'price' => $this->request->getVar('price'),
+        'expiry_date' => $this->request->getVar('expiry_date'),
+        'created_at' => $this->request->getVar('created_at'),
+        ];
+        $this->product->save($data);
+        return redirect()->to('product');
+    }
     public function product($product)
     {
         echo $product;
     }
     public function cessangeles()
     {
-        echo 'working';
+        $data['product'] = $this->product->findAll();
+        return view ('products', $data);
     }
     public function index()
     {
--- app/Views/products.php
+++ app/Views/products.php
@@ -5,6 +5,52 @@
     <title>Document</title>
 </head>
 <body>
+    <form action="/save" method="post">
+            <label>UPC</label>
+            <input type="text" name="UPC" placeholder="UPC">
+           <br>
+            <label>name</label>
+            <input type="text" name="name" placeholder="name" >
+            <br>
+            <label>quantity</label>
+            <input type="text" name="quantity" placeholder="quantity" >
+            <br>
+            <label>price</label>
+            <input type="text" name="price" placeholder="price">
+            <br>
+            <label>expiry_date</label>
+            <input type="text" name="expiry_date" placeholder="expiry_date">
+            <br>
+            <label>created_at</label>
+            <input type="text" name="created_at" placeholder="created_at">
+            <br>
+            <input type="submit" value="save">
+
+    </form>
     <h1>Product listing</h1>
+    <table border="1">
+        <tr>
+            <th>UPC</th>
+            <th>name</th>
+            <th>quantity</th>
+            <th>price</th>
+            <th>expiry_date</th>
+            <th>created_at</th>
+            <th>action</th>
+
+        </tr>
+        <?php foreach ($product as $pr): ?>
+            <tr>
+                <td><?= $pr['UPC'] ?></td>
+                <td><?= $pr['name'] ?></td>
+                <td><?= $pr['quantity'] ?></td>
+                <td><?= $pr['price'] ?></td>
+                <td><?= $pr['expiry_date'] ?></td>
+                <td><?= $pr['created_at'] ?></td>
+                <td><a href="/delete/<?= $pr['id']?>">delete</a> </td>
+            </tr>
+       
+            <?php endforeach; ?>
+    </table>
 </body>
 </html>
